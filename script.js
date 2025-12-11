012c058fc8af4fee850c46b4e8e4849bc469761f
Kayisha naidoo <naidookayisha8@gmail.com>
2025-12-11 15:56:47 +0200

    chore(add gif):

--- script.js
+++ script.js
@@ -118,7 +118,7 @@ document.addEventListener("DOMContentLoaded", function () {
     sparkle.className = "sparkle";
 
     // Random star symbol
-    const stars = ["💜", "🪐", "✨"];
+    const stars = ["🪐", "✨"];
     sparkle.textContent = stars[Math.floor(Math.random() * stars.length)];
 
     // Random size
